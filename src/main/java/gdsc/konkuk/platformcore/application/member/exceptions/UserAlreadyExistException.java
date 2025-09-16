37f66b1b504a7301c7fb2bb658c3e37090657302
chabin37 <7chabin@gmail.com>
2025-09-17 00:26:28 +0900

    refactor : error 반환 메세지 형태 개선

--- src/main/java/gdsc/konkuk/platformcore/application/member/exceptions/UserAlreadyExistException.java
+++ src/main/java/gdsc/konkuk/platformcore/application/member/exceptions/UserAlreadyExistException.java
@@ -13,7 +13,8 @@ public class UserAlreadyExistException extends BusinessException {
         return new UserAlreadyExistException(errorCode, errorCode.getLogMessage());
 
     }
+    // 유저 정보 중복시 어떤 정보가 중복되었는지 로그에 남기기 위한 메서드
     public static UserAlreadyExistException of(CustomErrorCode errorCode, String causeMember) {
-        return new UserAlreadyExistException(errorCode, errorCode.getLogMessage()+" "+causeMember);
+        return new UserAlreadyExistException(errorCode, errorCode.getLogMessage()+". "+causeMember);
     }
 }
